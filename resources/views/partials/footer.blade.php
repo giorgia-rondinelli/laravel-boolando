714111962e9fef5282d5b5079ee420ece8f420f0
giorgia-rondinelli <g.rondinelli4@gmail.com>
2024-05-09 16:23:34 +0200

    [add] footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,22 @@
+<footer>
+    <div class="foot contenitore ">
+      <div class="informazioni">
+        <h5>Boolando s.r.l</h5>
+        <div>
+          <span>Informazioni legali</span>
+          <span>Informativa sulla privacy</span>
+          <span>Diritto di recesso</span>
+        </div>
+      </div>
+      <div class="social">
+        <h6>Trovaci anche su</h6>
+        <div>
+          <i class="fa-brands fa-square-twitter"></i>
+          <i class="fa-brands fa-square-facebook"></i>
+          <i class="fa-brands fa-square-instagram"></i>
+          <i class="fa-brands fa-square-pinterest"></i>
+          <i class="fa-brands fa-youtube"></i>
+        </div>
+      </div>
+  </div>
+  </footer>
